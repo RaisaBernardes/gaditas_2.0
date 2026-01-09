2822930b2203ba5c858a2cdfc03710c2b7468b63
Raisa <raisa.rmro@gmail.com>
2026-01-09 18:50:58 -0300

    testemonial padding changes

--- src/components/ui/TestemonialSection.tsx
+++ src/components/ui/TestemonialSection.tsx
@@ -10,12 +10,12 @@ export default function TestimonialSection() {
 
       />
       
-      <section className="py-32 bg-white">
+      <section className="py-24 bg-white">
         <div className="max-w-[1024px] mx-auto px-6 md:px-10 text-center">
           <span className="material-symbols-outlined text-[var(--yellow)] opacity-70 block mb-8" style={{ fontSize: "120px" }}>
             format_quote
           </span>
-          <h3 className="text-3xl md:text-4xl lg:text-5xl font-serif text-[var(--black)] mb-10 max-w-5xl mx-auto merriweather-light leading-tight">
+          <h3 className="text-3xl md:text-4xl lg:text-4xl font-serif text-[var(--black)] mb-10 max-w-5xl mx-auto merriweather-light leading-tight">
             "Desde o primeiro contato, senti que estava lidando com uma empresa séria e comprometida. A equipe da Gaditas nos acompanhou em cada etapa da obra, sempre com transparência, organização e atenção aos detalhes. O resultado ficou além do que imaginávamos! Recomendo muito!"
           </h3>
           <div className="flex flex-col items-center gap-1">
